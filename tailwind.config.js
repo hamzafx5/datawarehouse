7c391036498584b8020856d5497266cf6e0bd5ec
hamzafx5 <bouchgarahamza@gmail.com>
2022-03-03 19:43:49 +0100

    seting up the main files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ["./dist/index.html"],
+	darkMode: "class",
+	theme: {
+		fontFamily: {
+			sans: ["Open Sans", "sans-serif"],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
